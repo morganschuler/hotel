7bc35b5954b202b647a694566afe19261a5f2391
Morgan Schuler <morganlschuler@gmail.com>
2019-09-23 11:56:20 -0700

    Updated logic in method to find available room in hotel controller

--- lib/hotel_controller.rb
+++ lib/hotel_controller.rb
@@ -8,8 +8,6 @@ module Hotel
 
     # Wave 1
     def initialize
-      # @reservation
-      @date_range
       @rooms = []
       @reservations = [] 
 
@@ -32,30 +30,59 @@ module Hotel
     end
 
     def reservations_by_date(date)
-      saved_reservations = @reservations.select do |reservation| #acts like map and creates a new array
-        date_range.in_date_range?(date) #could this be date_range - was reservation.date_range
+      reservations_by_date = []
+      @reservations.each do |reservation|
+        if reservation.date_range.in_date_range?(date)
+            reservations_by_date << reservation
+        end
       end
-      return saved_reservations
+      return reservations_by_date
     end
 
     def available_rooms_by_date_range(start_date, end_date)
-      given_range = DateRange.new(start_date, end_date)
-      unavailable_rooms = {}
-
+      reserved_rooms = []
       @reservations.each do |reservation|
-        if given_range.start_date < reservation.end_date && reservation.start_date < given_range.end_date #date_range.enddate used to be reservation.end_date
-          unavailable_rooms[reservation.room] = true
+        if reservation.date_range.dates_overlap?(start_date, end_date)
+          reserved_rooms << reservation.selected_room #room?
         end
       end
+      return reserved_rooms
+    end
 
-      available_rooms = @rooms.select do |room|
-        !unavailable_rooms[room]
+
+    def find_available_room(start_date, end_date)
+      available_rooms = []
+      @rooms.each do |room|
+        if !(available_rooms_by_date_range(start_date, end_date).include?(room))
+          available_rooms << room
+        end
       end
-
       return available_rooms
     end
 
   end
 end
 
+# def reservations_by_date(date)
+#   saved_reservations = @reservations.select do |reservation| #acts like map and creates a new array
+#     date_range.in_date_range?(date) #could this be date_range - was reservation.date_range
+#   end
+#   return saved_reservations
+# end
 
+# def available_rooms_by_date_range(start_date, end_date)
+#   given_range = DateRange.new(start_date, end_date)
+#   unavailable_rooms = {}
+
+#   @reservations.each do |reservation|
+#     if given_range.start_date < reservation.end_date && reservation.start_date < given_range.end_date #date_range.enddate used to be reservation.end_date
+#       unavailable_rooms[reservation.room] = true
+#     end
+#   end
+
+#   available_rooms = @rooms.select do |room|
+#     !unavailable_rooms[room]
+#   end
+
+#   return available_rooms
+# end
